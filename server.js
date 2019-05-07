f655689bd5cd5a1cd9b630c1abdf1bb704b91bd8
Mario Hercules <mariohercules@hotmail.com>
2019-05-07 01:54:37 -0300

    Refactory webservice calls

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var express_graphql = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+const courseService = require('./services/course.service');
+
 // GraphQL schema
 var schema = buildSchema(`
     type Query {
@@ -18,46 +20,13 @@ var schema = buildSchema(`
     }
 `);
 
-var coursesData = [
-    {
-        id: 1,
-        title: 'The Complete Node.js Developer Course',
-        author: 'Andrew Mead, Rob Percival',
-        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs/'
-    },
-    {
-        id: 2,
-        title: 'Node.js, Express & MongoDB Dev to Deployment',
-        author: 'Brad Traversy',
-        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
-    },
-    {
-        id: 3,
-        title: 'JavaScript: Understanding The Weird Parts',
-        author: 'Anthony Alicea',
-        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
-        topic: 'JavaScript',
-        url: 'https://codingthesmartway.com/courses/understand-javascript/'
-    }
-]
-var getCourse = function(args) { 
-    var id = args.id;
-    return coursesData.filter(course => {
-        return course.id == id;
-    })[0];
+var getCourse = function(args) {     
+    return courseService.filterCourse(args);
 }
-var getCourses = function(args) {
-    if (args.topic) {
-        var topic = args.topic;
-        return coursesData.filter(course => course.topic === topic);
-    } else {
-        return coursesData;
-    }
+var getCourses = function() {
+    return courseService.getAll();
 }
+
 var root = {
     course: getCourse,
     courses: getCourses
@@ -75,7 +44,7 @@ app.use('/graphql', express_graphql({
 // Create an express server and a REST endpoint
 app.get("/api/v1/courses", (req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
-    res.json(coursesData);
+    res.json(getCourses());
 });
 
 app.get("/api/v1/course/:id", (req, res, next) => {
--- soap-server.js
+++ soap-server.js
@@ -3,42 +3,13 @@ var http = require('http');
 var express = require('express');
 const bodyParser = require('body-parser');
 
-var coursesData = [
-    {
-        id: 1,
-        title: 'The Complete Node.js Developer Course',
-        author: 'Andrew Mead, Rob Percival',
-        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs/'
-    },
-    {
-        id: 2,
-        title: 'Node.js, Express & MongoDB Dev to Deployment',
-        author: 'Brad Traversy',
-        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
-    },
-    { 
-        id: 3,
-        title: 'JavaScript: Understanding The Weird Parts',
-        author: 'Anthony Alicea',
-        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
-        topic: 'JavaScript',
-        url: 'https://codingthesmartway.com/courses/understand-javascript/'
-    }
-] 
+const courseService = require('./services/course.service');
 
-var getCourse = function(args) { 
-    var id = args.id;
-    return coursesData.filter(course => {
-        return course.id == id;
-    })[0];
-} 
-
-var getCourses = function(args) {   
-    return coursesData;    
+var getCourse = function(args) {     
+    return courseService.filterCourse(args);
+}
+var getCourses = function(args) {
+    return courseService.getAll();
 }
 
 var myService = {
